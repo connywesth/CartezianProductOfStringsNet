4dbc687bbf726e842e89b906dc22218381c2aeb9
Conny Westh <3123534+connywesth@users.noreply.github.com>
2020-07-25 18:12:51 +0200

    Added the property folder.
    Added the property folder.

---
+++ CartezianProductOfStringsNetLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CartezianProductOfStringsNet")]
+[assembly: AssemblyDescription("CartezianProductOfStringsNet")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Adopter KB, Conny Westh")]
+[assembly: AssemblyProduct("CartezianProductOfStringsNet")]
+[assembly: AssemblyCopyright("Copyright © Adopter KB, Conny Westh 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b932a2ba-ed02-459e-846c-c20f15ac69d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
